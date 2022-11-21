58f6db139c8a935edd0020cf0d81215c1f9ce460
Jalal KADDOURI <jalalkadori2@gmail.com>
2022-11-21 14:54:00 +0100

    adding script

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-// club list selection 
+function redirect() {
+    location.replace('confirmation.html');
+   };
 
+function redirectback() {
+    location.replace('./index.html');
+   };
+
+
+function LastNameCheck(){
+    const x = document.getElementById("lName").value;
+    if(x.length < 30 && x =! ){
+        document.getElementById("lName").style.border = "1px solid #59CE8F";
+        document.getElementById("lNameOutput").innerHTML = "";
+    } else {
+        document.getElementById("lNameOutput").innerHTML = "* Le nom doit etre moins de 30 charactere";
+        document.getElementById("lName").style.border = "1px solid #DE0068";
+    }
+}
+
+function FirstNameCheck(){
+    const x = document.getElementById("fName").value;
+    if(x.length < 30 && x.length > 0){
+        document.getElementById("fName").style.border = "1px solid #59CE8F";
+        document.getElementById("fNameOutput").innerHTML = "";
+    } else {
+        document.getElementById("fNameOutput").innerHTML = "* Le nom doit etre moins de 30 charactere";
+        document.getElementById("fName").style.border = "1px solid #DE0068";
+    }
+}
